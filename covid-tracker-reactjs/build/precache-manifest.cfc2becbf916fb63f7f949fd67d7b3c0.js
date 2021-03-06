3f064e66c9921b9bdbbac2ca0bd291af7f47fc92
saintjohnie <42511508+saintjohnie@users.noreply.github.com>
2021-03-06 03:53:49 +0100

    first commit

---
+++ covid-tracker-reactjs/build/precache-manifest.cfc2becbf916fb63f7f949fd67d7b3c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c849fd4eb179ee9f6af669c312b6f519",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "191364e2e2eb8c205e8a",
+    "url": "/static/css/main.bf9e36d1.chunk.css"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/js/2.ad8e3933.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.ad8e3933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "191364e2e2eb8c205e8a",
+    "url": "/static/js/main.a222219a.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
